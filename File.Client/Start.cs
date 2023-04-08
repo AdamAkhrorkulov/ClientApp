69948d596a815295dc20a8b30040ed0452e330f0
Adam <akhrorkuloww@gmail.com>
2023-04-09 02:11:09 +0500

    done

--- File.Client/Start.cs
+++ File.Client/Start.cs
@@ -7,9 +7,9 @@
             var c = new BaseClient();
             var res = c.GetData("https://localhost:7177/File");
             var c2 = new BaseClient();
-            var res2 = c2.GetData("https://localhost:7177/File");
+            var res2 = c2.GetData("https://localhost:7177/Person");
             var c3 = new BaseClient();
-            var res3 = c3.GetData("https://localhost:7177/File");
+            var res3 = c3.GetData("https://localhost:7177/Student");
             Console.WriteLine(res);
             Console.WriteLine(res2);
             Console.WriteLine(res3);
